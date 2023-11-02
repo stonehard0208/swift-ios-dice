58ad8a47106127e56a7ab2ea402d50d5526e7086
Lisachen0208 <lisa@nixbiosensors.com>
2023-11-02 12:00:17 -0500

    dice app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix");
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewOne.image = [UIImage(imageLiteralResourceName: "DiceOne"),
+         UIImage(imageLiteralResourceName: "DiceTwo"),
+         UIImage(imageLiteralResourceName: "DiceThree"),
+         UIImage(imageLiteralResourceName: "DiceFour"),
+         UIImage(imageLiteralResourceName: "DiceFive"),
+                                  UIImage(imageLiteralResourceName: "DiceSix")][Int.random(in:0...5)]
+        
+        
+       
+    }
+    
 }
 
